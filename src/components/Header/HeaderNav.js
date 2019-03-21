085ba358237824966d403f81c1456575dcd42dd1
1n3ffbl3 <mizgier.m@gmail.com>
2019-03-21 09:49:12 +0100

    Refactoring HeaderNav component

--- src/components/Header/HeaderNav.js
+++ src/components/Header/HeaderNav.js
@@ -9,19 +9,26 @@ const HeaderNav = () => (
 				<NavLink
 					exact
 					className={styles.navItemLink}
+					activeClassName={styles.navItemLinkActive}
 					to="/">grocery list
 				</NavLink>
+			</li>
+			<li className={styles.navItem}>
 				<NavLink
 					className={styles.navItemLink}
+					activeClassName={styles.navItemLinkActive}
 					to="/checkout">check-out list
 				</NavLink>
+			</li>
+			<li className={styles.navItem}>
 				<NavLink
 					className={styles.navItemLink}
+					activeClassName={styles.navItemLinkActive}
 					to="/thingstosee">things to see
 				</NavLink>
 			</li>
 		</ul>
-	</nav>
+	</nav >
 );
 
 export default HeaderNav;
