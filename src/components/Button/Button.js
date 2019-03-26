4e9fa6cf76dbcd756dd2b605a8cc1bd52a8ce335
1n3ffbl3 <mizgier.m@gmail.com>
2019-03-26 11:22:07 +0100

    Adding PropTypes and conditional rendering to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
-const Button = ({ children, ...props }) => (
-	<button className={styles.button} {...props} >
-		{children}
-	</button>
-);
+const Button = ({ children, href, secondary, ...props }) => {
+
+	const buttonClass = secondary ? styles.secondary : styles.button;
+	return (
+		<>
+			{
+				href ? (
+					<a
+						href={href}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={buttonClass}
+					>
+						{children}
+					</a>
+				) : (
+						<button className={buttonClass} {...props} >
+							{children}
+						</button>
+					)
+			}
+		</>
+	);
+}
+
+
+
+Button.propTypes = {
+	href: PropTypes.string,
+	secondary: PropTypes.bool,
+};
+
+Button.defaultProps = {
+	href: null,
+	secondary: null,
+};
 
 export default Button;
